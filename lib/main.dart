b311ef3b30341ed99d5345e58ba248ee8675ac0e
Harsh Moradiya <harshcerti@gmail.com>
2020-01-14 23:58:53 +0530

    UT celebration

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,8 @@ class MyApp extends StatelessWidget {
         title: 'Flutter login demo',
         debugShowCheckedModeBanner: false,
         theme: new ThemeData(
-<<<<<<< HEAD
           primarySwatch: Colors.blue,
         ),
         home: new RootPage(auth: new Auth()));
   }
 }
-=======
-          primarySwatch: Colors.orange,
-        ),
-        home: new RootPage(auth: new Auth()));
-  }
-}
->>>>>>> 730c4f3a4fddb47db92e1308f29226c73a1d0fe9
